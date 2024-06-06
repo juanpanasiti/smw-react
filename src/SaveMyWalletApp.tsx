6e454124e9c07044bd813fe6bc0c3fd5075a7771
Juan Marcelo Panasiti <juanpanasiti@gmail.com>
2024-06-05 22:58:55 -0300

    Install and config tanstack query

--- src/SaveMyWalletApp.tsx
+++ src/SaveMyWalletApp.tsx
@@ -1,14 +1,27 @@
 import { BrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import { AppRouter } from './router';
 import { AppTheme } from './theme';
 
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            refetchOnWindowFocus: false,
+        },
+    },
+});
+
 export const SaveMyWalletApp = () => {
     return (
         <BrowserRouter>
-            <AppTheme>
-                <AppRouter />
-            </AppTheme>
+            <QueryClientProvider client={queryClient}>
+                <AppTheme>
+                    <AppRouter />
+                </AppTheme>
+                <ReactQueryDevtools initialIsOpen={false} />
+            </QueryClientProvider>
         </BrowserRouter>
     );
 };
