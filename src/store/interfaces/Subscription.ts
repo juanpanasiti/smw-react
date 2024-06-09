b8744afe0f08ab4d022203e8d13c89735a96d67a
Juan Marcelo Panasiti <juanpanasiti@gmail.com>
2024-06-09 19:16:22 -0300

    fix property type

--- src/store/interfaces/Subscription.ts
+++ src/store/interfaces/Subscription.ts
@@ -1,8 +1,10 @@
+import { ExpenseStatusEnum } from "../../wallet/enums";
+
 export interface Subscription {
     id: number;
     creditCardId: number;
     title: string;
     ccName: string;
     amount: number;
-    status: string; // TODO: replace with enum
+    status: ExpenseStatusEnum
 }
