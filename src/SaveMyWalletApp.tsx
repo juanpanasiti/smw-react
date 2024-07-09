511735611e18efeef1fcf251153e120eea07b682
Juan Marcelo Panasiti <juanpanasiti@gmail.com>
2024-07-09 19:35:05 -0300

    remove tanstack query dependency

--- src/SaveMyWalletApp.tsx
+++ src/SaveMyWalletApp.tsx
@@ -1,21 +1,14 @@
 import { BrowserRouter } from 'react-router-dom';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import { AppRouter } from './router';
 import { AppTheme } from './theme';
 
-const queryClient = new QueryClient();
-
 export const SaveMyWalletApp = () => {
     return (
         <BrowserRouter>
-            <QueryClientProvider client={queryClient}>
-                <AppTheme>
-                    <AppRouter />
-                </AppTheme>
-                <ReactQueryDevtools initialIsOpen={false} />
-            </QueryClientProvider>
+            <AppTheme>
+                <AppRouter />
+            </AppTheme>
         </BrowserRouter>
     );
 };
