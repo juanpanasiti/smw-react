6e89215ecf2dd58a59883627fcadbecb48b7592e
Juan Marcelo Panasiti <juanpanasiti@gmail.com>
2024-08-09 12:00:40 -0300

    Remove title

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -2,14 +2,11 @@ import React from 'react';
 
 interface Props {
     children: React.ReactNode;
-    title: string;
 }
 
-export const AuthLayout = ({ children, title }: Props) => {
+export const AuthLayout = ({ children }: Props) => {
     return (
         <>
-            <h1>{title}</h1>
-            <hr />
             <section>{children}</section>
         </>
     );
