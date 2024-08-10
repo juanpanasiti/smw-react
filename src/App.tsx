943607fbcc213ad79d1531377fdb701f3e13ff4c
Juan Marcelo Panasiti <juanpanasiti@gmail.com>
2024-08-10 00:47:14 -0300

    replace message store with notistack

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import { BrowserRouter } from 'react-router-dom';
 
 import { AppRouter } from './router';
 import { AppTheme } from './theme';
+import { SnackbarProvider } from 'notistack';
 
 export const App = () => {
     return (
         <BrowserRouter>
             <AppTheme>
-                <AppRouter />
+                <SnackbarProvider maxSnack={5} autoHideDuration={3000}>
+                    <AppRouter />
+                </SnackbarProvider>
             </AppTheme>
         </BrowserRouter>
     );
