bcd19bf606f68395c356ff0aa1742f338946aff2
Juan Marcelo Panasiti <juanpanasiti@gmail.com>
2024-12-13 15:14:03 -0300

    install and configure notistack

--- src/SaveMyWalletApp.tsx
+++ src/SaveMyWalletApp.tsx
@@ -1,13 +1,17 @@
 import { BrowserRouter } from 'react-router';
+import { SnackbarProvider } from 'notistack';
 
 import { AppRouter } from './router/AppRouter';
 import { AppTheme } from './theme';
 export const SaveMyWalletApp = () => {
     return (
         <BrowserRouter>
-          <AppTheme>
-              <AppRouter />
-          </AppTheme>
+            <AppTheme>
+                <SnackbarProvider />
+                <SnackbarProvider maxSnack={5} autoHideDuration={3000}>
+                    <AppRouter />
+                </SnackbarProvider>
+            </AppTheme>
         </BrowserRouter>
     );
 };
