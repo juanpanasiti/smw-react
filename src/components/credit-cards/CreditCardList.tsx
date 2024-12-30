2bcece0fb55d9e0a5c504b1074aa9bf083cad134
Juan Marcelo Panasiti <juanpanasiti@gmail.com>
2024-12-30 13:58:08 -0300

    add title

--- src/components/credit-cards/CreditCardList.tsx
+++ src/components/credit-cards/CreditCardList.tsx
@@ -1,10 +1,11 @@
-import { Box } from '@mui/material';
+import { useState } from 'react';
+
+import { Box, Typography } from '@mui/material';
 
 import { CreditCard } from '../../types';
 import { CreditCardCard } from './CreditCardCard';
 import { AddCreditCardButton } from './AddCreditCardButton';
 import { CreditCardModalForm } from './CreditCardModalForm';
-import { useState } from 'react';
 
 interface Props {
     cards: CreditCard[];
@@ -13,25 +14,28 @@ interface Props {
 export const CreditCardList = ({ cards }: Props) => {
     const [creditCardSelected, setCreditCardSelected] = useState<CreditCard | null>(null);
     return (
-        <Box
-            sx={{
-                display: 'flex',
-                flexWrap: 'wrap',
-                gap: '1rem',
-            }}
-        >
-            {cards.map((card) => (
-                <CreditCardCard key={card.id} creditCard={card} handleOnEditClick={() => setCreditCardSelected(card)} />
-            ))}
-            <AddCreditCardButton />
-            {creditCardSelected && (
-                <CreditCardModalForm
-                    open={!!creditCardSelected}
-                    handleClose={() => setCreditCardSelected(null)}
-                    card={creditCardSelected}
-                    ccId={creditCardSelected.id}
-                />
-            )}
-        </Box>
+        <>
+            <Typography variant='h2'>Tarjetas de Crédito</Typography>
+            <Box
+                sx={{
+                    display: 'flex',
+                    flexWrap: 'wrap',
+                    gap: '1rem',
+                }}
+            >
+                {cards.map((card) => (
+                    <CreditCardCard key={card.id} creditCard={card} handleOnEditClick={() => setCreditCardSelected(card)} />
+                ))}
+                <AddCreditCardButton />
+                {creditCardSelected && (
+                    <CreditCardModalForm
+                        open={!!creditCardSelected}
+                        handleClose={() => setCreditCardSelected(null)}
+                        card={creditCardSelected}
+                        ccId={creditCardSelected.id}
+                    />
+                )}
+            </Box>
+        </>
     );
 };
