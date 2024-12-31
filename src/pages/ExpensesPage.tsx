4a59f63de6458c3aa28372933382b043a4bd6562
Juan Marcelo Panasiti <juanpanasiti@gmail.com>
2024-12-31 16:20:19 -0300

    fix container styling

--- src/pages/ExpensesPage.tsx
+++ src/pages/ExpensesPage.tsx
@@ -1,4 +1,5 @@
 import { Divider, Typography } from '@mui/material';
+import styled from 'styled-components';
 
 import { CreditCardList } from '../components/credit-cards';
 import { ExpenseList } from '../components/expenses';
@@ -7,12 +8,18 @@ import { useWallet } from '../hooks';
 export const ExpensesPage = () => {
     const { creditCards, expenses } = useWallet();
     return (
-        <>
+        <ContainerFake>
             <Typography variant='h1'>Gastos</Typography>
             <Divider sx={{ marginY: 3 }} />
             <CreditCardList cards={creditCards} />
             <Divider sx={{ marginY: 3 }} />
             <ExpenseList expenses={expenses} />
-        </>
+        </ContainerFake>
     );
 };
+
+const ContainerFake = styled.div`
+    min-height: 100%;
+    display: flex;
+    flex-direction: column;
+`;
