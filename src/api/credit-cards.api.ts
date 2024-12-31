8d7808ef73de30530b520aa9ce61cebcaae850ad
Juan Marcelo Panasiti <juanpanasiti@gmail.com>
2024-12-31 15:41:35 -0300

    implement function to get all pages

--- src/api/credit-cards.api.ts
+++ src/api/credit-cards.api.ts
@@ -10,9 +10,27 @@ export const callGetCreditCardListApi = async (limit: number, offset: number): P
         limit,
         offset,
     };
-    const { data } = await apiClient.get<CreditCardResApi[]>(Endpoints.CREDIT_CARDS, { params: { queryParams } });
+    const { data } = await apiClient.get<CreditCardResApi[]>(Endpoints.CREDIT_CARDS, { params: { ...queryParams } });
     return data.map(parseCreditCardFromApi);
 };
+
+export const getAllCreditCardsApi = async (): Promise<CreditCard[]> => {
+    const limit = 100;
+    let offset = 0;
+    let allCreditCards: CreditCard[] = [];
+    let hasMore = true;
+
+    while (hasMore) {
+        const creditCards = await callGetCreditCardListApi(limit, offset);
+        allCreditCards = allCreditCards.concat(creditCards);
+
+        hasMore = creditCards.length === limit;
+        offset += limit;
+    }
+
+    return allCreditCards;
+};
+
 
 export const callCreateNewCreditCardApi = async (creditCardData: NewCreditCard): Promise<CreditCard> => {
     const { data } = await apiClient.post<CreditCardResApi>(Endpoints.CREDIT_CARDS, parseNewCreditCardToApi(creditCardData));
