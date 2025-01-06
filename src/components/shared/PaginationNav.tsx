6a147ad98ae1b58b41c02a3de65bce2d499bc318
Juan Marcelo Panasiti <juanpanasiti@gmail.com>
2025-01-06 13:36:02 -0300

    fix pagination component

--- src/components/shared/PaginationNav.tsx
+++ src/components/shared/PaginationNav.tsx
@@ -1,5 +1,5 @@
 import { Box, IconButton, Pagination as MuiPagination, PaginationItem } from '@mui/material';
-import { FirstPage, LastPage, KeyboardArrowLeft, KeyboardArrowRight } from '@mui/icons-material';
+import { FirstPage, LastPage } from '@mui/icons-material';
 import styled from 'styled-components';
 
 interface Props {
@@ -10,47 +10,21 @@ interface Props {
 
 const PaginationNav = ({ totalPages, currentPage, handlePageChange }: Props) => {
     return (
-        <Box sx={{ display: 'flex', justifyContent: 'center', marginTop: '10px' }}>
+        <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: '10px' }}>
+            <IconButton disabled={currentPage === 1 || totalPages === 0} onClick={() => handlePageChange(1)} sx={{ marginLeft: 2 }}>
+                <FirstPage />
+            </IconButton>
+
             <MuiPagination
                 count={totalPages}
                 page={currentPage}
                 onChange={(_, page) => handlePageChange(page)}
                 siblingCount={1}
                 boundaryCount={1}
-                renderItem={(item) => (
-                    <PaginationItemStyled
-                        // component="button"
-                        {...item}
-                        disabled={totalPages === 0}
-                    />
-                )}
+                renderItem={(item) => <PaginationItemStyled {...item} disabled={totalPages === 0} />}
             />
-            <IconButton
-                disabled={currentPage === 1 || totalPages === 0}
-                onClick={() => handlePageChange(1)}
-                sx={{ marginLeft: 2 }}
-            >
-                <FirstPage />
-            </IconButton>
-            <IconButton
-                disabled={currentPage === 1 || totalPages === 0}
-                onClick={() => handlePageChange(currentPage - 1)}
-                sx={{ marginLeft: 1 }}
-            >
-                <KeyboardArrowLeft />
-            </IconButton>
-            <IconButton
-                disabled={currentPage === totalPages || totalPages === 0}
-                onClick={() => handlePageChange(currentPage + 1)}
-                sx={{ marginLeft: 1 }}
-            >
-                <KeyboardArrowRight />
-            </IconButton>
-            <IconButton
-                disabled={currentPage === totalPages || totalPages === 0}
-                onClick={() => handlePageChange(totalPages)}
-                sx={{ marginLeft: 1 }}
-            >
+
+            <IconButton disabled={currentPage === totalPages || totalPages === 0} onClick={() => handlePageChange(totalPages)} sx={{ marginLeft: 1 }}>
                 <LastPage />
             </IconButton>
         </Box>
