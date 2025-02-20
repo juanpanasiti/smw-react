c252664917eafa1566ada9453da17fad08d463fd
Juan Marcelo Panasiti <juanpanasiti@gmail.com>
2025-02-20 17:10:29 -0300

    add prop to customize container

--- src/components/credit-cards/CreditCardContainer.tsx
+++ src/components/credit-cards/CreditCardContainer.tsx
@@ -1,8 +1,9 @@
-import { Card } from '@mui/material';
+import { Card, SxProps, Theme } from '@mui/material';
 
 interface Props {
     children: React.ReactNode;
+    sx?: SxProps<Theme>;
 }
-export const CreditCardContainer = ({ children }: Props) => {
-    return <Card sx={{ width: '350px', display: 'flex', flexDirection: 'column' }}>{children}</Card>;
+export const CreditCardContainer = ({ children, sx = {} }: Props) => {
+    return <Card sx={{ width: '350px', display: 'flex', flexDirection: 'column', ...sx }}>{children}</Card>;
 };
